a927ee09d12f1f7b521ed347eb4bbafe63fe4a80
hiren <72260974+hirensavani-99@users.noreply.github.com>
2022-08-10 01:38:45 +0200

    portfolio update v1

--- src/component/portfolio/Portfolio.js
+++ src/component/portfolio/Portfolio.js
@@ -1,76 +1,161 @@
 import React from 'react'
 import './Portfolio.css'
 import Image1 from '../../assests/readersclub.PNG'
+import { VanillaJavscriptProjects, ReactJsProjects, NodeJsProjects } from './projects'
+import JsImage from '../../assests/download.png'
+import ReactJsImage from '../../assests/reactjs.png'
+import Node from '../../assests/nodejs.webp'
+import NextJsLogo from '../../assests/nextjs.png'
+import MERN from '../../assests/MERN.png'
 export default function Portfolio() {
     return (
         <section id="portfolio">
             <h5>My Recent Work</h5>
             <h2>Portfolio</h2>
 
+
+            <div className='title__container'>
+                <h2>MERN stack</h2>
+                <img className='title__img' src={MERN} alt="" />
+            </div>
+
             <div className='container portfolio__container'>
-                <article className='portfolio__items'>
-                    <div className='portfolio__item-image'>
-                        <img src={Image1} alt="" />
-                    </div>
-                    <h3>portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href="github link" className='btn' traget="_blank">Github</a>
-                        <a href="live demo" className='btn btn-primary' traget="_blank">Live demo</a>
-                    </div>
 
-                </article>
-                <article className='portfolio__items'>
-                    <div className='portfolio__item-image'>
-                        <img src={Image1} alt="" />
-                    </div>
-                    <h3>portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href="github link" className='btn' traget="_blank">Github</a>
-                        <a href="live demo" className='btn btn-primary' traget="_blank">Live demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__items'>
-                    <div className='portfolio__item-image'>
-                        <img src={Image1} alt="" />
-                    </div>
-                    <h3>portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href="github link" className='btn' traget="_blank">Github</a>
-                        <a href="live demo" className='btn btn-primary' traget="_blank">Live demo</a>
-                    </div>
+                {
 
-                </article>
-                <article className='portfolio__items'>
-                    <div className='portfolio__item-image'>
-                        <img src={Image1} alt="" />
-                    </div>
-                    <h3>portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href="github link" className='btn' traget="_blank">Github</a>
-                        <a href="live demo" className='btn btn-primary' traget="_blank">Live demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__items'>
-                    <div className='portfolio__item-image'>
-                        <img src={Image1} alt="" />
-                    </div>
-                    <h3>portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href="github link" className='btn' traget="_blank">Github</a>
-                        <a href="live demo" className='btn btn-primary' traget="_blank">Live demo</a>
-                    </div>
+                    NodeJsProjects.map(project => {
+                        return (
+                            <article className='portfolio__items'>
+                                <div className='portfolio__item-image'>
+                                    <img src={project.picture} alt="" />
+                                </div>
+                                <h3>{project.name}</h3>
+                                <div className='portfolio__item-cta'>
+                                    <a href={project.github} className='btn' target="_blank">Github</a>
+                                    {project?.live && <a href={project?.live} className='btn btn-primary' target="_blank">Live demo</a>}
+                                </div>
 
-                </article>
-                <article className='portfolio__items'>
-                    <div className='portfolio__item-image'>
-                        <img src={Image1} alt="" />
-                    </div>
-                    <h3>portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href="github link" className='btn' traget="_blank">Github</a>
-                        <a href="live demo" className='btn btn-primary' traget="_blank">Live demo</a>
-                    </div>
-                </article>
+                            </article>
+                        )
+                    })
+                }
+
+
+            </div>
+
+
+            <div className='title__container'>
+                <h2>Next JS</h2>
+                <img className='title__img' src={NextJsLogo} alt="" />
+            </div>
+
+            <div className='container portfolio__container'>
+
+                {
+
+                    NodeJsProjects.map(project => {
+                        return (
+                            <article className='portfolio__items'>
+                                <div className='portfolio__item-image'>
+                                    <img src={project.picture} alt="" />
+                                </div>
+                                <h3>{project.name}</h3>
+                                <div className='portfolio__item-cta'>
+                                    <a href={project.github} className='btn' target="_blank">Github</a>
+                                    {project?.live && <a href={project?.live} className='btn btn-primary' target="_blank">Live demo</a>}
+                                </div>
+
+                            </article>
+                        )
+                    })
+                }
+
+
+            </div>
+            <div className='title__container'>
+                <h2>Node JS</h2>
+                <img className='title__img' src={Node} alt="" />
+            </div>
+
+            <div className='container portfolio__container'>
+
+                {
+
+                    NodeJsProjects.map(project => {
+                        return (
+                            <article className='portfolio__items'>
+                                <div className='portfolio__item-image'>
+                                    <img src={project.picture} alt="" />
+                                </div>
+                                <h3>{project.name}</h3>
+                                <div className='portfolio__item-cta'>
+                                    <a href={project.github} className='btn' target="_blank">Github</a>
+                                    {project?.live && <a href={project?.live} className='btn btn-primary' target="_blank">Live demo</a>}
+                                </div>
+
+                            </article>
+                        )
+                    })
+                }
+
+
+            </div>
+
+
+            <div className='title__container'>
+                <h2>React JS</h2>
+                <img className='title__img' src={ReactJsImage} alt="" />
+            </div>
+
+            <div className='container portfolio__container'>
+
+                {
+
+                    ReactJsProjects.map(project => {
+                        return (
+                            <article className='portfolio__items'>
+                                <div className='portfolio__item-image'>
+                                    <img src={project.picture} alt="" />
+                                </div>
+                                <h3>{project.name}</h3>
+                                <div className='portfolio__item-cta'>
+                                    <a href={project.github} className='btn' target="_blank">Github</a>
+                                    {project?.live && <a href={project?.live} className='btn btn-primary' target="_blank">Live demo</a>}
+                                </div>
+
+                            </article>
+                        )
+                    })
+                }
+
+
+            </div>
+
+            <div className='title__container'>
+                <h2>Vanilla JS</h2>
+                <img className='title__img' src={JsImage} alt="" />
+            </div>
+
+            <div className='container portfolio__container'>
+
+                {
+
+                    VanillaJavscriptProjects.map(project => {
+                        return (
+                            <article className='portfolio__items'>
+                                <div className='portfolio__item-image'>
+                                    <img src={project.picture} alt="" />
+                                </div>
+                                <h3>{project.name}</h3>
+                                <div className='portfolio__item-cta'>
+                                    <a href={project.github} className='btn' target="_blank">Github</a>
+                                    <a href={project.live} className='btn btn-primary' target="_blank">Live demo</a>
+                                </div>
+
+                            </article>
+                        )
+                    })
+                }
             </div>
 
         </section>
---
+++ src/component/portfolio/projects.js
@@ -0,0 +1,100 @@
+import TickTacToe from '../../assests/tic-tc-toe.PNG'
+import Photogallary from '../../assests/photogallery.PNG'
+import CssVar from '../../assests/cssVar.PNG'
+import Clock from '../../assests/Clock.PNG'
+import Drumapp from '../../assests/DrumApp.PNG'
+import ExpenseHandler from '../../assests/ExpenseHandler.PNG'
+import BurgerBuilder from '../../assests/BurgerBuilder.PNG'
+import BookStore from '../../assests/BookStore.PNG'
+import PortFolio from '../../assests/PortfolioSite.PNG'
+import BusApp from '../../assests/BusApp.PNG'
+import WeatherApp from '../../assests/WeatherApp.PNG'
+import Chatapp from '../../assests/chatapp.PNG'
+
+export const VanillaJavscriptProjects = [
+    {
+        name: "tic-tac-toe Game",
+        github: "https://github.com/hirensavani-99/tic-tac-toe.git",
+        live: "https://tic-tac-toe-gamma-topaz.vercel.app/",
+        picture: TickTacToe
+    }, {
+        name: "image-gallery",
+        github: "https://github.com/hirensavani-99/ImageGallery.git",
+        live: "https://image-gallery-green.vercel.app/",
+        picture: Photogallary
+    },
+    {
+        name: "css variable",
+        github: "https://github.com/hirensavani-99/cssVariables-.git",
+        live: "https://css-variables-bice.vercel.app/",
+        picture: CssVar
+    },
+    {
+        name: "Clock",
+        github: "https://github.com/hirensavani-99/clock.git",
+        live: "https://clock-psi-mauve.vercel.app/",
+        picture: Clock
+    },
+    {
+        name: "Drum",
+        github: "https://github.com/hirensavani-99/drum-Application.git",
+        live: "https://drum-application.vercel.app/",
+        picture: Drumapp
+    }
+]
+
+export const ReactJsProjects = [
+    {
+        name: "Expense Handler",
+        github: "https://github.com/hirensavani-99/ExpenseHandler.git",
+        live: "https://expense-handler.vercel.app/",
+        picture: ExpenseHandler
+    },
+    {
+        name: "Burger Builder",
+        github: "https://github.com/hirensavani-99/burgerBuilder.git",
+        live: "https://burger-builder-alpha-five.vercel.app/",
+        picture: BurgerBuilder
+    },
+    {
+        name: "Book Store",
+        github: "https://github.com/hirensavani-99/Book-Store.git",
+        live: "https://book-store-sigma.vercel.app/",
+        picture: BookStore
+    },
+    {
+        name: "Portfolio site",
+        github: "https://github.com/hirensavani-99/PortfolioSite.git",
+        live: "https://hirensavani.vercel.app/",
+        picture: PortFolio
+    },
+    {
+        name: "Bus Application",
+        github: "https://github.com/hirensavani-99/busservice.git",
+        picture: BusApp
+    }
+]
+
+export const NodeJsProjects = [
+    {
+        name: "Weather Application",
+        github: "https://github.com/hirensavani-99/Weather_app.git",
+        live: "https://kano-weather-app.herokuapp.com/",
+        picture: WeatherApp
+    },
+    {
+        name: "Real Time Chat Application",
+        github: "https://github.com/hirensavani-99/Talk.git",
+        live: "https://savani-talk.herokuapp.com/",
+        picture: Chatapp
+    }
+]
+
+
+export const NextJsProjects = [
+    {
+        name: "Twitter Clone",
+        github: "https://github.com/hirensavani-99/TwitterClone-.git",
+        picture: WeatherApp
+    }
+]
